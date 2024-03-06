b5793c2f94faa13cf8d00011ffaa914cbab04ecc
Creeper9132898 <125325853+Creeper9132898@users.noreply.github.com>
2024-03-06 08:34:00 +0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-def on_button_pressed_a():
-    basic.show_number(input.compass_heading())
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
 def on_button_pressed_ab():
-    basic.show_number(input.temperature())
+    basic.show_number(input.compass_heading())
+    if input.compass_heading() > 0 and input.compass_heading() < 45:
+        music.play(music.string_playable("C5 B A G F E D C ", 120),
+            music.PlaybackMode.UNTIL_DONE)
+    if input.compass_heading() > 45 and input.compass_heading() < 90:
+        music.play(music.string_playable("C5 A B G A F G E ", 120),
+            music.PlaybackMode.UNTIL_DONE)
+    if input.compass_heading() > 90 and input.compass_heading() < 135:
+        music.play(music.string_playable("C D E F G A B C5 ", 120),
+            music.PlaybackMode.UNTIL_DONE)
+    if input.compass_heading() > 135 and input.compass_heading() < 180:
+        music.play(music.string_playable("G B A G C5 B A B ", 120),
+            music.PlaybackMode.UNTIL_DONE)
+    if input.compass_heading() > 180 and input.compass_heading() < 225:
+        music.play(music.string_playable("E B C5 A B G A F ", 120),
+            music.PlaybackMode.UNTIL_DONE)
+    if input.compass_heading() > 225 and input.compass_heading() < 270:
+        music.play(music.string_playable("G B A G C5 B A B ", 120),
+            music.PlaybackMode.UNTIL_DONE)
+    if input.compass_heading() > 270 and input.compass_heading() < 315:
+        music.play(music.string_playable("A F E F D G E F ", 120),
+            music.PlaybackMode.UNTIL_DONE)
+    if input.compass_heading() > 315 and input.compass_heading() < 0:
+        music.play(music.string_playable("B A G A G F A C5 ", 120),
+            music.PlaybackMode.UNTIL_DONE)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
-
-def on_button_pressed_b():
-    basic.show_number(input.light_level())
-input.on_button_pressed(Button.B, on_button_pressed_b)
